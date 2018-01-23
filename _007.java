c886e472cc8bef8dc87e091b928f8230ef1e2bf2
jieniz <jieniz@andrew.cmu.edu>
2018-01-23 11:42:32 +1030

    solution one

---
+++ _007.java
@@ -0,0 +1,37 @@
+/** 
+Solution One: arraylist --> array --> sum
+Notes:
+1. in case of the overflow of sum: use long. if overflow, return 0;
+2. if x < 0, x = -x, return -positiveReverse(x);
+3. convert arraylist to array: Integer[] array = list.toArray(new Integer[list.size()]);
+4. range of 32-bit int: [-2147483648, 2147483647].
+*/
+
+class Solution {
+    public int reverse(int x) {
+        if(x >= 0){
+            return positiveReverse(x);
+        }else{
+            x = -x;
+            return -positiveReverse(x);
+        }
+    }
+    
+    public static int positiveReverse(int x){
+        List<Integer> list = new ArrayList<>();
+        while (x != 0){
+            list.add(x % 10);
+            x = x / 10;
+        }
+        Integer[] array = list.toArray(new Integer[list.size()]);
+        long sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i] * Math.pow(10, array.length-1-i);
+        }
+        if(sum > 2147483647 || sum < -2147483648){
+            return 0;
+        }else{
+            return (int)sum;
+        }
+    }
+}
