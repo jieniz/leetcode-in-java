3c27955587995334c1ba98d82356241c97ffd540
jieniz <31397624+jieniz@users.noreply.github.com>
2018-06-14 14:41:49 +0800

    Update 016_3SumClosest.java

--- 016_3SumClosest.java
+++ 016_3SumClosest.java
@@ -1,3 +1,8 @@
+/**
+双指针
+排序数组，固定指针front，移动middle和tail找到最接近的和（每次移动前先判断是否更新sum的值）
+*/
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
@@ -10,9 +15,9 @@ class Solution {
             while(middle < tail){
                 if(Math.abs(target - sum) > Math.abs(target - nums[front] - nums[middle] - nums[tail])){
                     sum = nums[front] + nums[middle] + nums[tail];
-                }
+                }//判断是否需要更新sum的值
                 
-                if(nums[middle] + nums[tail] < target - nums[front]){
+                if(nums[middle] + nums[tail] < target - nums[front]){//同2Sum
                     middle++;  
                 }else if(nums[middle] + nums[tail] > target - nums[front]){
                     tail--;
