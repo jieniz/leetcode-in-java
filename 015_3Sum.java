ce23ffcf9641aab7f7f7d25c30050cd649e18e0a
jieniz <31397624+jieniz@users.noreply.github.com>
2018-06-14 10:42:00 +0800

    Update 015_3Sum.java

--- 015_3Sum.java
+++ 015_3Sum.java
@@ -1,5 +1,10 @@
 /**
 双指针
+数组排序，设置3个指针，固定其中1个，移动剩余2个（同2sum）
+Notes：
+1. 每个数组中可以有重复元素，但是返回的list中不能有重复数组，因此需要针对3个指针进行去重；
+2. 找到正确的middle、tail后要实现Middle++和tail--，否则陷入无限循环（此处在去重中实现了指针的加减）
+3. while语句中的三个if不能并列，要试用if, else if，else的方式，避免执行完一个还符合另一个条件
 */
 
 class Solution {
@@ -14,7 +19,7 @@ class Solution {
             
             while(middle < tail){//同2sum
                 if(nums[middle] + nums[tail] == - nums[front]){
-                    list.add(Arrays.asList(nums[front], nums[middle], nums[tail]));
+                    list.add(Arrays.asList(nums[front], nums[middle], nums[tail]));//添加嵌套list
                     
                     int tempMiddle = nums[middle];
                     int tempTail = nums[tail];
