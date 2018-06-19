b89623275f7aac277510b1b1ad332ddad3e2fb98
jieniz <31397624+jieniz@users.noreply.github.com>
2018-06-19 17:09:32 +0800

    Update and rename _011.java to 011_ContainerWithMostWater.java

--- 011_ContainerWithMostWater.java
+++ 011_ContainerWithMostWater.java
@@ -1,5 +1,5 @@
 /**
- SOLUTION: two pointers
+ SOLUTION: two pointers 双指针
  NOTES:
  1. for any i, the maximum area will be the farest j with height[j] > height[i];
  2. O(n)遍历：由于面积由较小的值决定，因此移动较大值对面积没有影响，仅移动较小值就可以实现遍历，避免双重遍历，复杂度为O(n).
@@ -7,17 +7,20 @@
 
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int max = 0;
-        while (left < right){
-            max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
-            if(height[left] < height[right]){
+        int left = 0, right = height.length - 1;
+        int temp, volumn, maxVolumn = 0;
+        
+        while(left < right){
+            temp = height[left] > height[right] ? height[right] : height[left];
+            volumn = temp * (right - left);
+            if(volumn > maxVolumn) maxVolumn = volumn;
+            
+            if(height[left] <= height[right]){
                 left++;
             }else{
                 right--;
             }
         }
-        return max;
+        return maxVolumn;
     }
 }
