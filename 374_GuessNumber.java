31d9c2a6f89acc83e0782a6cb9db309c258464d1
jieniz <31397624+jieniz@users.noreply.github.com>
2018-07-05 18:34:43 +0800

    Update 374_GuessNumber.java

--- 374_GuessNumber.java
+++ 374_GuessNumber.java
@@ -1,4 +1,7 @@
-
+/**
+题目：给定上限n，根据guess方法返回值猜数字
+方法：二分查找 --> 1到n自带排序
+*/
 
 /* The guess API is defined in the parent class GuessGame.
    @param num, your guess
@@ -9,7 +12,8 @@ public class Solution extends GuessGame {
     public int guessNumber(int n) {
         int low = 1, high = n, mid = 1;
         while(low < high){
-            mid = low + (high - low) / 2;
+            mid = low + (high - low) / 2;//若n的数值很大，low+high可能导致溢出，所以采取这种写法！
+           
             if(guess(mid) == 0){
                 break;
             }else if(guess(mid) == -1){
