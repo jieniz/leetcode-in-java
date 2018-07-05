ed0e47958ca446d5f0bc46676782dabf0ba7978e
jieniz <31397624+jieniz@users.noreply.github.com>
2018-07-05 18:31:30 +0800

    Create 374_GuessNumber.java

---
+++ 374_GuessNumber.java
@@ -0,0 +1,23 @@
+
+
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1, high = n, mid = 1;
+        while(low < high){
+            mid = low + (high - low) / 2;
+            if(guess(mid) == 0){
+                break;
+            }else if(guess(mid) == -1){
+                high = --mid;
+            }else if(guess(mid) == 1){
+                low = ++mid;    
+            }
+        }
+        return mid;
+    }
+}
